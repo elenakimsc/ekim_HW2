e7facd69bbaa2597c534da4a505f86d03f9dc08f
L <elf.kim@mail.ru>
2021-12-19 01:59:22 +0300

    Added functions

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+FILEPATH = 'input.txt'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def read_array(filepath):
+    file = open(filepath, 'r')
+    line = file.readline()
+    array = list(map(int, line.split()))
+    return array
 
 
-# Press the green button in the gutter to run the script.
+def max_(array):
+    if len(array) == 0:
+        print('Array must contain elements')
+        return None
+    m1 = array[0]
+    for item in array:
+        if item > m1:
+            m1 = item
+    return m1
+
+
+def min_(array):
+    if len(array) == 0:
+        print('Array must contain elements')
+        return None
+    m1 = array[0]
+    for item in array:
+        if item < m1:
+            m1 = item
+    return m1
+
+
+def sum_(array):
+    s = 0
+    for item in array:
+        s += item
+    return s
+
+
+def multiply(array):
+    m = 1
+    for item in array:
+        m *= item
+    return m
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    arr = read_array(FILEPATH)
+    print(min_(arr))
+    print(max_(arr))
+    print(sum_(arr))
+    print(multiply(arr))
